786632e31763cccfc7fa6ccb26efc1656ff04c73
郭威 <apanly@163.com>
2019-11-27 10:17:23 +0800

    guowei --ls

--- www/modules/merchant/views/staff/index/index.php
+++ www/modules/merchant/views/staff/index/index.php
@@ -1,5 +1,7 @@
 <?php
 use \common\services\GlobalUrlService;
-
+use \common\components\helper\StaticAssetsHelper;
+StaticAssetsHelper::includeAppJsStatic( GlobalUrlService::buildWwwStaticUrl("/js/merchant/staff/index/index.js"),www\assets\MerchantAsset::className() );
+StaticAssetsHelper::includeAppCssStatic( GlobalUrlService::buildWwwStaticUrl(""),www\assets\MerchantAsset::className() )
 ?>
 
