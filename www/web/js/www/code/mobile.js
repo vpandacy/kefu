3540c2f988ba493f70ae9988ce0f861fc4e3a6c1
Adolph <1138027478@qq.com>
2019-12-13 17:41:30 +0800

    add 修改js

--- www/web/js/www/code/mobile.js
+++ www/web/js/www/code/mobile.js
@@ -1,9 +1,21 @@
 ;
-$('.icon-zaixianzixun').click(function () {
-    $('.waponline-max').removeClass('dis_none')
-    $('.icon-zaixianzixun').addClass('dis_none')
-})
-$('.icon-zuojiantou').click(function () {
-    $('.waponline-max').addClass('dis_none')
-    $('.icon-zaixianzixun').removeClass('dis_none')
-})
+var mobile_ops ={
+    init: function () {
+        this.eventBind();
+    },
+    eventBind: function () {
+        $('.icon-zaixianzixun').click(function () {
+            $('.waponline-max').removeClass('dis_none')
+            $('.icon-zaixianzixun').addClass('dis_none')
+        });
+        
+        $('.icon-zuojiantou').click(function () {
+            $('.waponline-max').addClass('dis_none')
+            $('.icon-zaixianzixun').removeClass('dis_none')
+        })
+    }
+};
+
+$(document).ready(function () {
+    mobile_ops.init();
+});
