9a56ddb6d7e5c65cae372e2b136d54314df481b4
Adolph <1138027478@qq.com>
2019-12-16 14:35:56 +0800

    add 添加验证码类

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -29,21 +29,56 @@ return [
         "guest" => [
             "name" => "guest",
             'domain' => '.kefu.dev.hsh568.cn',
-        ]
+        ],
     ],
     'guest' => [
         "register" => [
-            "host" => '0.0.0.0:1238',
+            "host" => '0.0.0.0:8210',
             'name' => 'guest_register'
         ],
         'gateway' => [
-            "host" => '0.0.0.0:8282',
+            "host" => '0.0.0.0:8230',
+            'ip' => 'www.kefu.test.hsh568.cn',
+            'port' => '8230',
+            'start_port' => 8231 ,
             "name" => 'guest_gateway',
-            "register_host" => "0.0.0.0:1238"
+            "register_host" => "0.0.0.0:8210",
         ],
         'busi_worker' => [
             "name" => 'guest_busworker',
-            "register_host" => "0.0.0.0:1238"
+            "register_host" => "0.0.0.0:8210",
+            "inner" => [
+                "host" => "0.0.0.0:8220",
+                "name" => "guest_transfer"
+            ]
+        ],
+        'push' => [
+            'host' => '127.0.0.1:8220'
+        ]
+    ],
+    'cs' => [
+        "register" => [
+            "host" => '0.0.0.0:9210',
+            'name' => 'cs_register'
+        ],
+        'gateway' => [
+            "host" => '0.0.0.0:9230',
+            'ip' => 'www.kefu.test.hsh568.cn',
+            'port' => '9230',
+            'start_port' => 9231 ,
+            "name" => 'cs_gateway',
+            "register_host" => "0.0.0.0:9210",
+        ],
+        'busi_worker' => [
+            "name" => 'cs_busworker',
+            "register_host" => "0.0.0.0:9210",
+            "inner" => [
+                "host" => "0.0.0.0:9220",
+                "name" => "cs_transfer"
+            ]
+        ],
+        'push' => [
+            'host' => '127.0.0.1:9220'
         ]
     ]
 ];
