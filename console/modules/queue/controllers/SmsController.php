779010f47091ea795da42e7c2fb8361677e62b44
Adolph <1138027478@qq.com>
2019-12-16 17:45:16 +0800

    del 删除报警信息

--- console/modules/queue/controllers/SmsController.php
+++ console/modules/queue/controllers/SmsController.php
@@ -17,17 +17,6 @@ class SmsController extends  BaseController {
 			return $this->echoLog( 'no data to handle ~~' );
 		}
 
-		//查询未发送的短信数量，超过设定值触发警报
-        $sms_config =  \Yii::$app->params['sms'];
-		$sms_warning = $sms_config['pending_warning'] ? $sms_config['pending_warning'] : 100;
-		$pending_count = QueueSms::find()->where(['status' => -2])->count();
-
-		if ($pending_count > $sms_warning) {
-		    $title = '短信警报通知';
-		    $content = "短信警报<br/>待发送短信已超出预设值{$sms_warning}<br/>当前待发送数量为{$pending_count}<br/>{请及时查看并处理}";
-//			MailService::addEmailQueue( $title,$content, $sms_config['warning_to']);
-		}
-
         foreach( $list as $_sms_info ) {
             $this->echoLog("queue_id:{$_sms_info['id']}");
             $_sms_info->status = -1;
