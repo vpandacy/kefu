762a1cce1911aa3523731b5b98ef3155076d8755
Adolph <1138027478@qq.com>
2019-12-23 14:54:08 +0800

    add 添加类型

---
+++ console/modules/chat/controllers/MonitorController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace console\modules\chat\controllers;
+
+use common\services\monitor\WSCenterService;
+use console\controllers\BaseController;
+use uc\services\UCConstantService;
+
+class MonitorController extends BaseController
+{
+    /**
+     * php yii chat/monitor/scrapy
+     * 获取网关，然后根据网关获取gateway 和 busi worker
+     */
+    public function actionScrapy()
+    {
+        $params = \Yii::$app->params;
+        $guest_config = [];
+        $cs_config = [];
+        foreach ($params as $_key => $_item ){
+            if( mb_stripos( $_key,"guest_" ) !== false ){
+                $guest_config[ $_key ] = $_item['register'];
+            }
+            if( mb_stripos( $_key,"cs_" ) !== false ){
+                $cs_config[ $_key ] = $_item['register'];
+            }
+        }
+        //获取register地址
+        foreach ( $guest_config as $_key => $_item ){
+            $tmp_params = $_item;
+            $tmp_params['type'] = UCConstantService::$ws_register;
+            WSCenterService::setKFWS( $tmp_params );
+        }
+
+        foreach ( $cs_config as $_key => $_item ){
+            $tmp_params = $_item;
+            $tmp_params['type'] = UCConstantService::$ws_register;
+            WSCenterService::setKFWS( $tmp_params );
+        }
+        return $this->echoLog( "ok" );
+    }
+}
