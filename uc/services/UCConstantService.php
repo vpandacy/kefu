fc274844dc77cb293ade3c28c8e3979d3cdf210d
Adolph <1138027478@qq.com>
2019-12-23 21:06:28 +0800

    add 修改聊天状态

--- uc/services/UCConstantService.php
+++ uc/services/UCConstantService.php
@@ -5,5 +5,21 @@ use common\services\BaseService;
 
 class UCConstantService extends BaseService
 {
+    /**
+     * 注册.
+     * @var int
+     */
+    public static $ws_register = 1;
 
+    /**
+     * 网关.
+     * @var int
+     */
+    public static $ws_gateway = 2;
+
+    /**
+     * 业务worker.
+     * @var int
+     */
+    public static $ws_busiworker = 3;
 }
