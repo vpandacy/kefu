c2f7758cdf004fb07f158b4993c6baa1962a623b
Adolph <1138027478@qq.com>
2019-12-23 09:40:06 +0800

    add 添加历史的消息记录

--- www/modules/cs/controllers/VisitorController.php
+++ www/modules/cs/controllers/VisitorController.php
@@ -348,14 +348,36 @@ class VisitorController extends BaseController
         // 最后一次聊天的时间.
         $last_time = $this->post('last_time',date('Y-m-d H:i:s'));
 
+        $uuid = $this->post('uuid', '');
+
+        if(!$uuid) {
+            return $this->renderJSON([],'暂无信息');
+        }
+
         $guest_chat_log = GuestChatLog::find()
             ->where(['<','created_time', $last_time])
-            ->andWhere(['merchant_id'=>$this->getMerchantId(),'cs_id'=> $this->getStaffId()])
+            ->andWhere(['merchant_id'=>$this->getMerchantId(),'uuid'=> $uuid])
             ->orderBy(['id'=>SORT_DESC])
             ->limit($this->page_size)
             ->asArray()
             ->select(['id','cs_id','uuid','from_id','content','created_time'])
             ->all();
+
+        if($guest_chat_log) {
+            $customers = ModelHelper::getDicByRelateID($guest_chat_log, Staff::className(), 'cs_id','id',['nickname','avatar']);
+
+            foreach($guest_chat_log as $key=>$log) {
+                $log['staff_name'] = isset($customers[$log['cs_id']])
+                    ? $customers[$log['cs_id']]['nickname']
+                    : '暂无';
+
+                $log['cs_avatar'] = isset($customers[$log['cs_id']])
+                    ? GlobalUrlService::buildPicStaticUrl('hsh',$customers[$log['cs_id']]['avatar'])
+                    : GlobalUrlService::buildPicStaticUrl('hsh',ConstantService::$default_avatar);
+
+                $guest_chat_log[$key] = $log;
+            }
+        }
 
         return $this->renderJSON($guest_chat_log,'获取成功');
     }
