cd3e2a9dc3a70a2c58b26b6e5f4118faa98fb326
Adolph <1138027478@qq.com>
2019-12-23 15:04:29 +0800

    add 更换客服代码

--- www/views/default/index.php
+++ www/views/default/index.php
@@ -1,10 +1,10 @@
 <div><h1>测试聊天</h1></div>
 <script>
     (function() {
-        var _hshcode = document.createElement("script"),
+        var _ht_kf_code = document.createElement("script"),
             s = document.getElementsByTagName("script")[0];
 
-        _hshcode.src = "<?=\common\services\GlobalUrlService::buildKFUrl("/falkspyx/code/index");?>";
-        s.parentNode.insertBefore(_hshcode, s);
+        _ht_kf_code.src = "http://www.kefu.test.hsh568.cn/mlfpbss0/code/index";
+        s.parentNode.insertBefore(_ht_kf_code, s);
     })();
 </script>
