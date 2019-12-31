d3bf290d5b9487f6b6f8e0afa8dd6b55c23d5680
Adolph <1138027478@qq.com>
2019-12-31 17:38:33 +0800

    add 获取请求信息

--- www/web/js/www/code/socket.common.js
+++ www/web/js/www/code/socket.common.js
@@ -525,5 +525,25 @@
         }
     }
 
+    // 获取请求信息.
+    socket.prototype.getRequest = function (key, default_value){
+        var url = location.search; //获取url中"?"符后的字串
+        var theRequest = {};
+        if (url.indexOf("?") !== -1) {
+            var str = url.substr(1);
+            var strs = str.split("&");
+            for (var i = 0; i < strs.length; i++) {
+                theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+            }
+        }
+
+        if(key) {
+            return theRequest[key] ? theRequest[key] : default_value;
+        }
+
+        return theRequest;
+    };
+
+
     window.socket = socket;
 })(window);
