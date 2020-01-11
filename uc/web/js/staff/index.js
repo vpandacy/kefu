2d5f74075ddd7bde37a96779c13b9d037f9f6399
Adolph <1138027478@qq.com>
2020-01-11 22:18:30 +0800

    add 修改表头

--- uc/web/js/staff/index.js
+++ uc/web/js/staff/index.js
@@ -29,7 +29,7 @@ var uc_staff_index_ops = {
                     ,{field:'nickname', title: '昵称', templet: function (row) {
                         return row.nickname ? row.nickname : '暂无';
                     }}
-                    ,{field:'mobile', title: '手机号', width: 130, templet: function (row) {
+                    ,{field:'mobile', title: '手机号', templet: function (row) {
                         return row.mobile ? row.mobile : '暂无';
                     }} //minWidth：局部定义当前单元格的最小宽度，layui 2.2.1 新增
                     ,{field:'department', title: '所属部门',width:100}
