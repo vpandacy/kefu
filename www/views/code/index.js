d135c7204a0c87124bb60c8109bb1893bf7b5b4a
vincent <apanly@163.com>
2020-03-02 15:21:30 +0800

    guowei -- 修改JS

--- www/views/code/index.js
+++ www/views/code/index.js
@@ -1,50 +1,51 @@
 <?php if($is_mobile):?>
-    window.onload = function () {
-        var dynamicLoading = {
-            online: function () {
-                var iframe = document.createElement('iframe');
+!function(win, doc) {
+    "use strict";
+    var dynamicLoading = {
+        online: function () {
+            var iframe = document.createElement('iframe');
 
-                iframe.src = '<?=$url?>';
+            iframe.src = '<?=$url?>';
 
-                function load() {
-                    // 传参过去.然后根据参数来获取就可以了.
-                    iframe.contentWindow.postMessage(JSON.stringify({
-                        href : location.href,
-                        rf: document.referrer,
-                        title: document.title
-                    }), iframe.src);
-                }
-
-                if (iframe.attachEvent) {
-                    iframe.attachEvent('onload', load);
-                } else {
-                    iframe.onload  = load;
-                }
-
-                iframe.setAttribute('scrolling','no');
-
-                iframe.style['min-height'] = '100%';
-                iframe.style['height'] = '100%';
-                iframe.style['min-width'] = '100%';
-                iframe.style['width'] = '100%';
-                iframe.style['z-index'] = '9999';
-                iframe.style['position'] = 'fixed';
-                iframe.style['top'] = '0';
-                iframe.style['bottom'] = '0';
-                iframe.style['right'] = '0';
-                iframe.style['left'] = '0';
-                iframe.style['border'] = 'none';
-
-                var body = document.getElementsByTagName('body')[0];
-
-                body.appendChild(iframe);
+            function load() {
+                // 传参过去.然后根据参数来获取就可以了.
+                iframe.contentWindow.postMessage(JSON.stringify({
+                    href : location.href,
+                    rf: document.referrer,
+                    title: document.title
+                }), iframe.src);
             }
-        };
 
-        dynamicLoading.online();
+            if (iframe.attachEvent) {
+                iframe.attachEvent('onload', load);
+            } else {
+                iframe.onload  = load;
+            }
+
+            iframe.setAttribute('scrolling','no');
+
+            iframe.style['min-height'] = '100%';
+            iframe.style['height'] = '100%';
+            iframe.style['min-width'] = '100%';
+            iframe.style['width'] = '100%';
+            iframe.style['z-index'] = '9999';
+            iframe.style['position'] = 'fixed';
+            iframe.style['top'] = '0';
+            iframe.style['bottom'] = '0';
+            iframe.style['right'] = '0';
+            iframe.style['left'] = '0';
+            iframe.style['border'] = 'none';
+
+            var body = document.getElementsByTagName('body')[0];
+
+            body.appendChild(iframe);
+        }
     };
+    dynamicLoading.online();
+}(window, document);
 <?php else:?>
-window.onload = function () {
+!function(win, doc) {
+    "use strict";
     var dynamicLoading = {
         online: function () {
             var iframe = document.createElement('iframe');
@@ -85,7 +86,6 @@ window.onload = function () {
             body.appendChild(iframe);
         }
     };
-
     dynamicLoading.online();
-};
+}(window, document);
 <?php endif;?>
